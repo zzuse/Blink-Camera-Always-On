8f1f389148e91be4117a1ce29c924d89285a3f0b
zzuse <zzuse@outlook.com>
2023-12-31 22:18:19 -0600

    calibrate for my BlinkApp

--- Blink_Active_Feed.py
+++ Blink_Active_Feed.py
@@ -1,22 +1,23 @@
-continue_button_x_value = ENTER VALUE HERE
+## in the right half of the screen
+continue_button_x_value = 1915
 ##########################################
-continue_button_y_value = ENTER VALUE HERE 
+continue_button_y_value = 744
 ##########################################
-continue_button_red_value = ENTER VALUE HERE 
+continue_button_red_value = 23
 ##########################################
-continue_button_green_value = ENTER VALUE HERE 
+continue_button_green_value = 8
 ##########################################
-continue_button_blue_value = ENTER VALUE HERE 
+continue_button_blue_value = 4
 ##########################################
-play_button_x_value = ENTER VALUE HERE 
+play_button_x_value = 1918
 ##########################################
-play_button_y_value = ENTER VALUE HERE 
+play_button_y_value = 450
 ##########################################
-play_button_red_value = ENTER VALUE HERE 
+play_button_red_value = 25
 ##########################################
-play_button_green_value = ENTER VALUE HERE 
+play_button_green_value = 9
 ##########################################
-play_button_blue_value = ENTER VALUE HERE
+play_button_blue_value = 5
 ##########################################
 # Do not touch below text unless you know what you are doing! #
 ##########################################
@@ -33,4 +34,4 @@ while True:
     elif playButton == True:
         pyautogui.click(play_button_x_value, play_button_y_value)
     else:
-       time.sleep(5) 
+       time.sleep(5)
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 A python script to always keep a Blink camera on
 
 Blink cameras, owned and produced by Amazon, do not have an option to keep the camera's feed running without user input every minute. Furthermore, Blink does not officially have support for Windows, Mac OS, or Linux. This repository has instruction on how to get the Blink app up and running on any PC as well as keeping the live-feed from the cameras rolling 24/7. To begin, check the "Instructions" document for step-by-step instructions on what to do and how to use the python scripts in the repository.
+
+# Tested on Macos Ventura
+python 3.11.6; configured on automaticly vertical splited screen, and Blink App on the right side, it is work.
+`````````
+```sh
+pip3 install pyautogui
+pip3 install pynput
+python3 Monitor_Calibration_Tool.py
+python3 Blink_Active_Feed.py
+```
